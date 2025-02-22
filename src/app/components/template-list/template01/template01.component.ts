d355de2cf3af4430ad073bd1d67780c98590f0b0
Jeremiah Agarins <jagarin@seamfix.com>
2025-02-22 07:12:11 +0100

    update

---
+++ src/app/components/template-list/template01/template01.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template01',
+  imports: [],
+  templateUrl: './template01.component.html',
+  styleUrl: './template01.component.css'
+})
+export class Template01Component {
+
+}
